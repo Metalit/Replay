814162eca8a8529e99c9aa53a3ec396a94ebad11
Metalit <48568911+Metalit@users.noreply.github.com>
2022-07-07 18:37:14 -0500

    Improve frame replay combo

--- src/ReplayHooks.cpp
+++ src/ReplayHooks.cpp
@@ -8,7 +8,9 @@
 
 #include "GlobalNamespace/Saber.hpp"
 #include "GlobalNamespace/SaberType.hpp"
-#include "GlobalNamespace/ComboUIController.hpp"
+#include "GlobalNamespace/CutScoreBuffer.hpp"
+#include "GlobalNamespace/ComboController.hpp"
+#include "GlobalNamespace/NoteCutInfo.hpp"
 #include "GlobalNamespace/ScoreController.hpp"
 #include "GlobalNamespace/ScoreModel.hpp"
 #include "GlobalNamespace/GameEnergyCounter.hpp"
@@ -20,6 +22,7 @@
 #include "GlobalNamespace/SinglePlayerLevelSelectionFlowCoordinator.hpp"
 #include "GlobalNamespace/LevelCompletionResults.hpp"
 #include "UnityEngine/Transform.hpp"
+#include "System/Action_1.hpp"
 #include "System/Action_2.hpp"
 
 using namespace GlobalNamespace;
@@ -111,12 +114,57 @@ MAKE_HOOK_MATCH(ScoreController_LateUpdate, &ScoreController::LateUpdate, void, 
 }
 
 // override combo for frame replays
-MAKE_HOOK_MATCH(ComboUIController_HandleComboDidChange, &ComboUIController::HandleComboDidChange, void, ComboUIController* self, int combo) {
+MAKE_HOOK_MATCH(ComboController_HandlePlayerHeadDidEnterObstacles, &ComboController::HandlePlayerHeadDidEnterObstacles, void, ComboController* self) {
 
-    if(Manager::replaying && Manager::currentReplay.type == ReplayType::Frame)
-        combo = Manager::Frames::GetScoreFrame()->combo;
+    if(Manager::replaying && Manager::currentReplay.type == ReplayType::Frame && Manager::Frames::GetScoreFrame()->combo > 0)
+        return;
+
+    ComboController_HandlePlayerHeadDidEnterObstacles(self);
+}
+MAKE_HOOK_MATCH(ComboController_HandleNoteWasCut, &ComboController::HandleNoteWasCut, void, ComboController* self, NoteController* noteController, ByRef<NoteCutInfo> noteCutInfo) {
+
+    bool speedOK = noteCutInfo->speedOK;
+    bool directionOK = noteCutInfo->directionOK;
+    bool saberTypeOK = noteCutInfo->saberTypeOK;
+    bool wasCutTooSoon = noteCutInfo->wasCutTooSoon;
+
+    if(Manager::replaying && Manager::currentReplay.type == ReplayType::Frame) {
+        int combo = Manager::Frames::GetScoreFrame()->combo;
+        if(combo > 0) {
+            self->combo = combo - 1;
+            noteCutInfo->speedOK = true;
+            noteCutInfo->directionOK = true;
+            noteCutInfo->saberTypeOK = true;
+            noteCutInfo->wasCutTooSoon = false;
+        } else {
+            noteCutInfo->speedOK = false;
+            noteCutInfo->directionOK = false;
+            noteCutInfo->saberTypeOK = false;
+            noteCutInfo->wasCutTooSoon = true;
+        }
+    }
+    ComboController_HandleNoteWasCut(self, noteController, noteCutInfo);
     
-    ComboUIController_HandleComboDidChange(self, combo);
+    noteCutInfo->speedOK = speedOK;
+    noteCutInfo->directionOK = directionOK;
+    noteCutInfo->saberTypeOK = saberTypeOK;
+    noteCutInfo->wasCutTooSoon = wasCutTooSoon;
+}
+MAKE_HOOK_MATCH(ComboController_HandleNoteWasMissed, &ComboController::HandleNoteWasMissed, void, ComboController* self, NoteController* noteController) {
+    
+    auto gameplayType = noteController->noteData->gameplayType;
+
+    if(Manager::replaying && Manager::currentReplay.type == ReplayType::Frame) {
+        int combo = Manager::Frames::GetScoreFrame()->combo;
+        if(combo > 0) {
+            self->combo = combo;
+            noteController->noteData->gameplayType = NoteData::GameplayType::Bomb;
+            self->comboDidChangeEvent->Invoke(combo);
+        }
+    }
+    ComboController_HandleNoteWasMissed(self, noteController);
+    
+    noteController->noteData->gameplayType = gameplayType;
 }
 
 // override energy for frame replays
@@ -164,7 +212,9 @@ namespace Hooks {
         INSTALL_HOOK(logger, PlayerTransforms_Update);
         INSTALL_HOOK(logger, ScoreController_DespawnScoringElement);
         INSTALL_HOOK(logger, ScoreController_LateUpdate);
-        INSTALL_HOOK(logger, ComboUIController_HandleComboDidChange);
+        INSTALL_HOOK(logger, ComboController_HandlePlayerHeadDidEnterObstacles);
+        INSTALL_HOOK(logger, ComboController_HandleNoteWasCut);
+        INSTALL_HOOK(logger, ComboController_HandleNoteWasMissed);
         INSTALL_HOOK(logger, GameEnergyCounter_ProcessEnergyChange);
         INSTALL_HOOK(logger, HapticFeedbackController_PlayHapticFeedback);
         INSTALL_HOOK(logger, SinglePlayerLevelSelectionFlowCoordinator_HandleStandardLevelDidFinish);
