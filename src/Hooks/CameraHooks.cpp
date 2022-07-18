b1ed2381f8b550094c637a9de5efa2a7be6376c4
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-07-18 11:52:33 -0400

    Shenanigans

--- src/Hooks/CameraHooks.cpp
+++ src/Hooks/CameraHooks.cpp
@@ -6,12 +6,23 @@
 
 using namespace GlobalNamespace;
 
-#include "UnityEngine/Matrix4x4.hpp"
 #include "GlobalNamespace/LightManager.hpp"
+#include "GlobalNamespace/CoreGameHUDController.hpp"
+
+#include "UnityEngine/Matrix4x4.hpp"
 #include "UnityEngine/Camera.hpp"
 #include "UnityEngine/Transform.hpp"
+#include "UnityEngine/GameObject.hpp"
+#include "UnityEngine/CameraClearFlags.hpp"
+#include "UnityEngine/HideFlags.hpp"
+#include "UnityEngine/DepthTextureMode.hpp"
+#include "hollywood/shared/Hollywood.hpp"
+#include "UnityEngine/Time.hpp"
+#include "UnityEngine/Resources.hpp"
+#include "UnityEngine/AudioListener.hpp"
+#include "UnityEngine/StereoTargetEyeMask.hpp"
 
-UnityEngine::Matrix4x4 MatrixTranslate(UnityEngine::Vector3 vector) {
+constexpr UnityEngine::Matrix4x4 MatrixTranslate(UnityEngine::Vector3 const& vector) {
     UnityEngine::Matrix4x4 result;
     result.m00 = 1;
     result.m01 = 0;
@@ -32,31 +43,28 @@ UnityEngine::Matrix4x4 MatrixTranslate(UnityEngine::Vector3 vector) {
     return result;
 }
 
+UnityEngine::Camera *renderCamera;
+UnityEngine::Camera *mainCamera;
+
+UnityEngine::Transform *renderCameraTransform;
+
 MAKE_HOOK_MATCH(LightManager_OnCameraPreRender, &LightManager::OnCameraPreRender, void, LightManager* self, UnityEngine::Camera* camera) {
+    // if (!Manager::replaying || Manager::paused || Manager::Camera::mode != Manager::Camera::Mode::HEADSET)
+    //     return LightManager_OnCameraPreRender(self, camera);
+
+    // if (camera != renderCamera)
+    //     return;
 
     LightManager_OnCameraPreRender(self, camera);
 
-    if(Manager::replaying && !Manager::paused && Manager::GetSongTime() >= 0 && Manager::Camera::mode != Manager::Camera::Mode::HEADSET) {
-        camera->get_transform()->set_position(Manager::Camera::GetHeadPosition());
-        camera->get_transform()->set_rotation(Manager::Camera::GetHeadRotation());
-        
-        using cullingMatrixType = function_ptr_t<void, UnityEngine::Camera *, UnityEngine::Matrix4x4>;
-        auto set_cullingMatrix = *((cullingMatrixType) il2cpp_functions::resolve_icall("UnityEngine.Camera::set_cullingMatrix_Injected"));
 
-        set_cullingMatrix(camera, UnityEngine::Matrix4x4::Ortho(-99999, 99999, -99999, 99999, 0.001f, 99999) *
-            MatrixTranslate(UnityEngine::Vector3::get_forward() * -99999 / 2) * camera->get_worldToCameraMatrix());
+    if(Manager::replaying && !Manager::paused && Manager::GetSongTime() >= 0 && Manager::Camera::mode != Manager::Camera::Mode::HEADSET) {
+        camera->set_position(Manager::Camera::GetHeadPosition());
+        camera->set_rotation(Manager::Camera::GetHeadRotation());
     }
 }
 
-#include "GlobalNamespace/CoreGameHUDController.hpp"
-#include "UnityEngine/GameObject.hpp"
-#include "UnityEngine/CameraClearFlags.hpp"
-#include "UnityEngine/HideFlags.hpp"
-#include "UnityEngine/DepthTextureMode.hpp"
-#include "hollywood/shared/Hollywood.hpp"
-#include "UnityEngine/Time.hpp"
-#include "UnityEngine/Resources.hpp"
-#include "UnityEngine/AudioListener.hpp"
+
 
 Hollywood::AudioCapture* audioCapture = nullptr;
 
@@ -65,41 +73,62 @@ MAKE_HOOK_MATCH(CoreGameHUDController_Start, &CoreGameHUDController::Start, void
 
     CoreGameHUDController_Start(self);
 
-    if(Manager::replaying && Manager::Camera::rendering) {
-        auto mainCamera = UnityEngine::Camera::get_main();
-        // mainCamera->set_enabled(false);
+    renderCamera = mainCamera = UnityEngine::Camera::get_main();
 
-        auto customCamera = UnityEngine::Object::Instantiate(mainCamera);
-        // UnityEngine::Object::DontDestroyOnLoad(customCamera);
-        customCamera->set_enabled(true);
 
-        while (customCamera->get_transform()->get_childCount() > 0)
-            UnityEngine::Object::DestroyImmediate(customCamera->get_transform()->GetChild(0)->get_gameObject());
-        UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("CameraRenderCallbacksManager"));
-        UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("AudioListener"));
-        UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("MeshCollider"));
-        
-        customCamera->set_clearFlags(mainCamera->get_clearFlags());
-        customCamera->set_nearClipPlane(mainCamera->get_nearClipPlane());
-        customCamera->set_farClipPlane(mainCamera->get_farClipPlane());
-        customCamera->set_cullingMask(mainCamera->get_cullingMask());
-        customCamera->set_backgroundColor(mainCamera->get_backgroundColor());
-        customCamera->set_hideFlags(mainCamera->get_hideFlags());
-        customCamera->set_depthTextureMode(mainCamera->get_depthTextureMode());
-        // Makes the camera render before the main
-        customCamera->set_depth(mainCamera->get_depth() - 1);
-        
+
+    if (Manager::replaying && Manager::Camera::rendering) {
         Hollywood::CameraRecordingSettings settings{};
-        Hollywood::SetCameraCapture(customCamera, settings)->Init(settings);
 
-        UnityEngine::Time::set_captureDeltaTime(1.0f / settings.fps);
-
-        if(!settings.movieModeRendering) {
-            auto audioListener = UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::AudioListener*>().First([](auto x) { return x->get_gameObject()->get_activeInHierarchy(); });
+        // audio
+        if (!settings.movieModeRendering)
+        {
+            auto audioListener = UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::AudioListener *>().First([](auto x)
+                                                                                                                    { return x->get_gameObject()->get_activeInHierarchy(); });
             audioCapture = Hollywood::SetAudioCapture(audioListener);
             audioCapture->OpenFile("/sdcard/audio.wav");
         }
+        else // video
+        {
+            // mainCamera->set_enabled(false);
+
+            // auto customCameraObject = UnityEngine::Object::Instantiate(mainCamera->get_gameObject(), mainCamera->get_transform());
+            // auto customCamera = renderCamera = customCameraObject->GetComponent<UnityEngine::Camera*>();
+            auto customCamera = renderCamera = UnityEngine::Object::Instantiate(mainCamera);
+            // UnityEngine::Object::DontDestroyOnLoad(customCamera);
+            customCamera->set_enabled(true);
+            mainCamera->set_enabled(false);
+
+            while (customCamera->get_transform()->get_childCount() > 0)
+                UnityEngine::Object::DestroyImmediate(customCamera->get_transform()->GetChild(0)->get_gameObject());
+            // TODO: Are these needed?
+            // UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("CameraRenderCallbacksManager"));
+            // UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("AudioListener"));
+            // UnityEngine::Object::DestroyImmediate(customCamera->GetComponent("MeshCollider"));
+
+            customCamera->set_clearFlags(mainCamera->get_clearFlags());
+            customCamera->set_nearClipPlane(mainCamera->get_nearClipPlane());
+            customCamera->set_farClipPlane(mainCamera->get_farClipPlane());
+            customCamera->set_cullingMask(mainCamera->get_cullingMask());
+            customCamera->set_backgroundColor(mainCamera->get_backgroundColor());
+            customCamera->set_hideFlags(mainCamera->get_hideFlags());
+            customCamera->set_depthTextureMode(mainCamera->get_depthTextureMode());
+            // Makes the camera render before the main
+            customCamera->set_depth(mainCamera->get_depth() - 1);
+
+            Hollywood::SetCameraCapture(customCamera, settings)->Init(settings);
+
+            UnityEngine::Time::set_captureDeltaTime(1.0f / settings.fps);
+
+            using cullingMatrixType = function_ptr_t<void, UnityEngine::Camera *, UnityEngine::Matrix4x4>;
+            static auto set_cullingMatrix = *((cullingMatrixType)il2cpp_functions::resolve_icall("UnityEngine.Camera::set_cullingMatrix_Injected"));
+
+            // set_cullingMatrix(renderCamera, UnityEngine::Matrix4x4::Ortho(-99999, 99999, -99999, 99999, 0.001f, 99999) *
+            //                                   MatrixTranslate(UnityEngine::Vector3::get_forward() * -99999 / 2) * mainCamera->get_worldToCameraMatrix());
+        }
     }
+
+    renderCameraTransform = renderCamera->get_transform();
 }
 
 #include "GlobalNamespace/ResultsViewController.hpp"
